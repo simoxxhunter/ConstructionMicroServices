0bd3544c040987f21eca475e31866753235787c2
simoxxhunter <kingstonegame@gmail.com>
2024-09-01 21:42:24 +0100

    creation de projet

---
+++ construction/src/test/java/com/micro/construction/ConstructionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.construction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
