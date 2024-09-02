410817b1de5e0ba49214052aef8374eb24fbd479
simoxxhunter <kingstonegame@gmail.com>
2024-09-02 22:24:19 +0100

    ajoute method find by id tache

--- taskservice/src/main/java/com/micro/taskservice/Repository/TachesRepository.java
+++ taskservice/src/main/java/com/micro/taskservice/Repository/TachesRepository.java
@@ -3,5 +3,8 @@ package com.micro.taskservice.Repository;
 import com.micro.taskservice.Model.Taches;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TachesRepository extends JpaRepository<Taches, Long> {
+    List<Taches> findByIdProjet(Long idProjet);
 }
