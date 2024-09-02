8b80f4ba0d73b31096469ba62e898f1ebc056611
simoxxhunter <kingstonegame@gmail.com>
2024-09-02 20:53:51 +0100

    config eureka

---
+++ taskservice/src/test/java/com/micro/taskservice/TaskserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.taskservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
