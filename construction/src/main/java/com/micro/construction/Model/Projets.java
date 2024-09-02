833bf494a97fa8b78c0609df9118aefb1bbd9b87
simoxxhunter <kingstonegame@gmail.com>
2024-09-02 20:57:32 +0100

    creation projet model

---
+++ construction/src/main/java/com/micro/construction/Model/Projets.java
@@ -0,0 +1,27 @@
+package com.micro.construction.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Projets {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String description;
+    private LocalDate date_debut;
+    private LocalDate date_fin;
+    private Double budget;
+}
