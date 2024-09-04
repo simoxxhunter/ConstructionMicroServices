3a63176cf7f43163553da2c5a8deb781a9c9f1db
simoxxhunter <kingstonegame@gmail.com>
2024-09-04 23:08:04 +0100

    activation d'eureka

---
+++ utilisateurservice/src/test/java/com/micro/utilisateurservice/UtilisateurserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.utilisateurservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilisateurserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
