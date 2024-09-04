a2a8a614fedb18471cdb266777bdfea94731f9a3
simoxxhunter <kingstonegame@gmail.com>
2024-09-04 17:36:35 +0100

    creation de projet pour gerer les utulisateurs

---
+++ utilisateurs/src/main/java/com/micro/utilisateurs/UtilisateursApplication.java
@@ -0,0 +1,13 @@
+package com.micro.utilisateurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilisateursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilisateursApplication.class, args);
+	}
+
+}
