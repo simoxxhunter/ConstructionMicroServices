fa7020f53333bc2b9cd06c1b25a192cdaf18b1b3
simoxxhunter <kingstonegame@gmail.com>
2024-09-05 09:32:11 +0100

    configuration de controller

---
+++ utilisateurservice/src/main/java/com/micro/utilisateurservice/auth/AuthenticationRequest.java
@@ -0,0 +1,20 @@
+package com.micro.utilisateurservice.auth;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
+
+public class AuthenticationRequest {
+
+    private String email  ;
+    private String password;
+}
---
+++ utilisateurservice/src/main/java/com/micro/utilisateurservice/auth/AuthenticationResponse.java
@@ -0,0 +1,19 @@
+package com.micro.utilisateurservice.auth;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
+public class AuthenticationResponse {
+    private String token;
+    private String role;
+
+}
