34257c755e7b323dffd7c963b6c6444dd470f7dd
simoxxhunter <kingstonegame@gmail.com>
2024-09-05 14:58:38 +0100

    gateway route validator

---
+++ gateway/src/main/java/com/micro/Filter/RouteValidator.java
@@ -0,0 +1,24 @@
+package com.micro.Filter;
+
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+@Component
+public class RouteValidator {
+
+    public static final List<String> openApiEndPoint = List.of (
+            "/api/v1/auth/register",
+            "/api/v1/auth/authenticate",
+            "/eureka"
+    );
+
+    public Predicate<ServerHttpRequest> isSecred =
+            request -> openApiEndPoint
+                    .stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+
+}
