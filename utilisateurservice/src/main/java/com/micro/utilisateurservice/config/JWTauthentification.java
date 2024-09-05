d4ba13f69c4d22d2028e1b229e20c7435a9116d7
simoxxhunter <kingstonegame@gmail.com>
2024-09-05 09:33:25 +0100

    modification jwt

--- utilisateurservice/src/main/java/com/micro/utilisateurservice/config/JWTauthentification.java
+++ utilisateurservice/src/main/java/com/micro/utilisateurservice/config/JWTauthentification.java
@@ -1,6 +1,6 @@
 package com.micro.utilisateurservice.config;
 
-import com.project.HelpDesk.service.JwtService;
+import com.micro.utilisateurservice.service.*;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
